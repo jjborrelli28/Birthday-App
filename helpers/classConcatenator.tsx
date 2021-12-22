a02aeffe7a96b7a23c048b45b7d06e4bc154fdbc
Juan <juanjoseborrelli92@outlook.com>
2021-12-22 09:30:26 -0300

    Fix cc function

--- helpers/classConcatenator.tsx
+++ helpers/classConcatenator.tsx
@@ -1,3 +1,5 @@
 export function cc(...classNames: (string | boolean)[]) {
-  return classNames.join(" ");
+  return classNames
+    .filter((className) => typeof className === "string")
+    .join(" ");
 }
