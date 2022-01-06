3d80f9ea0006437ff8cb2a654496c54645c4a9de
Juan <juanjoseborrelli92@outlook.com>
2022-01-06 16:27:13 -0300

    Fix useLoginRedirect

--- temporal/useLoginRedirect.tsx
+++ temporal/useLoginRedirect.tsx
@@ -4,15 +4,13 @@ import { useEffect } from "react";
 export const useLoginRedirect = (router: NextRouter) => {
   //Login simulation
 
-  let logged;
-
   useEffect(() => {
     if (typeof window !== "undefined") {
-      logged = localStorage.getItem("logged") ?? false;
+      const logged = localStorage.getItem("logged") ?? false;
 
       if (!logged) {
         router.push("/login");
       }
     }
-  }, [logged]);
+  }, []);
 };
