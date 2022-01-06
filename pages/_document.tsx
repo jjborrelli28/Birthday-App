5310516efd76af8fa5154f8b94b55c90a1849ea8
Juan <juanjoseborrelli92@outlook.com>
2022-01-06 16:22:23 -0300

    Delete font awesome link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
-          <link
-            rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
-            integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
-            crossOrigin="anonymous"
-          ></link>
         </Head>
         <body>
           <Main />
